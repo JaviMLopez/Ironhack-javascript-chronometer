b8568d360ab8d792d230fdd7491ab328f978f815
Lopez Javier <20203@columbushs.com>
2020-01-09 12:53:47 -0500

    WIP

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,8 +1,20 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
+  }
+  getMinutes() {
+   return Math.floor(this.currentTime/60);
+  }
+  
+  getSeconds() {
+  return Math.floor(this.currentTime % 60);
+  }
   // twoDigitsNumber() {}
   // stopClick() {}
   // resetClick() {}
